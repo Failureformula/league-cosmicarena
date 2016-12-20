5ad85fff2857508d6f7e535bf2c7fb9b3961b4c6
Si_Ru <failureformula7750@naver.com>
2016-12-21 05:35:45 +0900

    ???

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+  	title: 'Express',
+  	data: 'CosmicArena League Web Test' 
+  });
 });
 
 module.exports = router;
